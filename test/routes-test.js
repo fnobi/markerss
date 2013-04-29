312d9becdd2b7dc2e224e988e9e647eeb8736d71
Fujisawa Shin <fujisawa-shin@in.kayac.com>
2013-04-29 12:20:05 +0900

    modelとconfigの設置、test utilの設置。(commit漏れ)

--- test/routes-test.js
+++ test/routes-test.js
@@ -3,14 +3,29 @@ var routes = require('../routes');
 require('should');
 
 describe('routes', function () {
-    var req = {};
-    var res = {
-        redirect: function () { },
-        render : function () { }
-    };
+    var req, res;
+
+    beforeEach(function () {
+        req = {};
+        res = {
+            redirect: function () { },
+            render : function () { }
+        };
+    });
 
     describe('index', function () {
-        it('should display index with posts', function (done) {
+        it('should display index with title', function (done) {
+            res.render = function (view, vars) {
+                view.should.equal('index');
+                vars.title.should.eql('Express');
+                done();
+            };
+            routes.index(req, res);
+        });
+    });
+
+    describe('index', function () {
+        it('should display index with title', function (done) {
             res.render = function (view, vars) {
                 view.should.equal('index');
                 vars.title.should.eql('Express');
